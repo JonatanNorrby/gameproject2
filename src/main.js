5d1befe22d510459192cdef4831ba79b87c513c7
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-14 17:52:20 +0200

    Use separate captain menu and bump v18

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { Game, UI } from './features/squadFormation.js';
+import { Game, UI } from './features/captainMenu.js';
 import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 
-const GAME_VERSION = 17;
+const GAME_VERSION = 18;
 const canvas = document.querySelector('#game-canvas');
 const touchStick = document.querySelector('#touch-stick');
 const ui = new UI();
