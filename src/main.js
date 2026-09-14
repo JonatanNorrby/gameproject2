6eab8c4666b6d6cb555124b72b64da1004f785a6
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-14 21:44:38 +0200

    Bump game version to v26

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Game, UI } from './features/squadConnectivity.js';
 import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 
-const GAME_VERSION = 25;
+const GAME_VERSION = 26;
 const canvas = document.querySelector('#game-canvas');
 const touchStick = document.querySelector('#touch-stick');
 const ui = new UI();
