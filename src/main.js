f216591390e139063c8ec2d5c5b139a74e093e0f
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-14 21:48:20 +0200

    Update runtime version to v27

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Game, UI } from './features/squadConnectivity.js';
 import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 
-const GAME_VERSION = 26;
+const GAME_VERSION = 27;
 const canvas = document.querySelector('#game-canvas');
 const touchStick = document.querySelector('#touch-stick');
 const ui = new UI();
