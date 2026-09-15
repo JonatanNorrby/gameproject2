e5a40cfa89794280c70a1a58bfc058fdcd784708
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-15 15:38:19 +0200

    Bump build to v52

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Game, UI } from './features/performanceAndVisualFixes.js';
 import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 
-const GAME_VERSION = 51;
+const GAME_VERSION = 52;
 const canvas = document.querySelector('#game-canvas');
 const touchStick = document.querySelector('#touch-stick');
 const ui = new UI();
