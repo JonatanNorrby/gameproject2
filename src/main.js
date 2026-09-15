39777693775c05b7770d22080d7cf6971dd91c86
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-15 11:21:34 +0200

    Activate support units v41

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { Game, UI } from './features/captainWeaponsAndDamageFlash.js';
+import { Game, UI } from './features/supportUnits.js';
 import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 
-const GAME_VERSION = 40;
+const GAME_VERSION = 41;
 const canvas = document.querySelector('#game-canvas');
 const touchStick = document.querySelector('#touch-stick');
 const ui = new UI();
