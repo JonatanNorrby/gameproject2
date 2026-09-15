41d263ec37c2ff9a0a7a4227c2bb9a47b21c26cd
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-15 16:43:43 +0200

    Bump build for issues 25 and 26

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 import { resetUnlockProgress } from './data/unlocks.js';
 import { resetMetaUpgradeProgress } from './data/metaUpgrades.js';
 
-const GAME_VERSION = 59;
+const GAME_VERSION = 60;
 const canvas = document.querySelector('#game-canvas');
 const touchStick = document.querySelector('#touch-stick');
 const ui = new UI();
