5d5a4b45033085152603a4558c72155e87209684
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-15 15:43:16 +0200

    Greatly reduce reinforcement upgrade frequency

--- src/systems/ProgressionSystem.js
+++ src/systems/ProgressionSystem.js
@@ -1,7 +1,7 @@
 import { GAME_BALANCE, RARITIES, UPGRADES } from '../data/content.js';
 import { createUnitModifierState } from '../data/unitModifiers.js';
 
-const REINFORCEMENT_CHOICE_WEIGHT = 1.05;
+const REINFORCEMENT_CHOICE_WEIGHT = 0.15;
 const STAT_CHOICE_WEIGHT = 1;
 
 export class ProgressionSystem {
