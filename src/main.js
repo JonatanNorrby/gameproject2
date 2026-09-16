f115fc1791b46e437faf81369dbd805a1d1c84f2
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-16 17:59:58 +0200

    Bump build to v91 for issue 48

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import {
 } from './data/metaUpgrades.js';
 import { getSpritePortraitSources } from './data/sprites.js';
 
-const GAME_VERSION = 90;
+const GAME_VERSION = 91;
 const BOOT_ASSET_TIMEOUT_MS = 4500;
 const BOOT_MINIMUM_VISIBLE_MS = 420;
 
