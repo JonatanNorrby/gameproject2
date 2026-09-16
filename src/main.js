53d5130b92564b9e0c963dccba3dd6ac0e65b085
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-16 17:03:25 +0200

    Bump game version to v85

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 import { getSpritePortraitSources } from './data/sprites.js';
 
-const GAME_VERSION = 84;
+const GAME_VERSION = 85;
 const BOOT_ASSET_TIMEOUT_MS = 4500;
 const BOOT_MINIMUM_VISIBLE_MS = 420;
 
