08125907392d8f9e956fbcafea928e255206cc3d
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-16 17:44:02 +0200

    Wire debug mob spawner and bump to v89

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Game, UI } from './features/treasureChests.js';
+import { Game, UI } from './features/debugMobSpawner.js';
 import { Input } from './core/Input.js';
 import { CAPTAINS, GAME_BALANCE } from './data/content.js';
 import {
@@ -7,7 +7,7 @@ import {
 } from './data/metaUpgrades.js';
 import { getSpritePortraitSources } from './data/sprites.js';
 
-const GAME_VERSION = 88;
+const GAME_VERSION = 89;
 const BOOT_ASSET_TIMEOUT_MS = 4500;
 const BOOT_MINIMUM_VISIBLE_MS = 420;
 
