6aa4f9d826c9a3966e0aa6abbbaa4de6843d4c43
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-16 13:22:48 +0200

    Bump build to v74 for Sniper unit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { getSpritePortraitSources } from './data/sprites.js';
 import { resetUnlockProgress } from './data/unlocks.js';
 import { resetPermanentProgression } from './data/metaUpgrades.js';
 
-const GAME_VERSION = 73;
+const GAME_VERSION = 74;
 const BOOT_ASSET_TIMEOUT_MS = 4500;
 const BOOT_MINIMUM_VISIBLE_MS = 420;
 
