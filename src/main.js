94e166974e97c4a1d46ca1524d33704afde6e75a
JonatanNorrby <123446373+JonatanNorrby@users.noreply.github.com>
2026-09-17 14:46:22 +0200

    Bump game version to v100 for issue #126

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {
 } from './data/metaUpgrades.js';
 import { getSpritePortraitSources } from './data/sprites.js';
 
-const GAME_VERSION = 99;
+const GAME_VERSION = 100;
 const BOOT_ASSET_TIMEOUT_MS = 4500;
 const BOOT_MINIMUM_VISIBLE_MS = 420;
 
